9c37c067d1cafb8171c0ece034ec9d6f28ab15f8
Avinash <apatil@kraususa.com>
2013-10-11 01:14:59 +0530

    New Files

---
+++ WPF Webcam/ViewModels/mBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KrausGRA.ViewModels
+{
+    /// <summary>
+    /// Avi: Box Model that holds information of box
+    /// </summary>
+   public class mBox
+    {
+
+       public String BoxNumber { get; set; }
+
+    }
+}
