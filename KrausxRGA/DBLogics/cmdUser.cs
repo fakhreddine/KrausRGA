2c2fe82fdbb853b529c36270377157da22f9e433
Avinash <apatil@kraususa.com>
2013-11-01 17:46:18 +0530

    User Command for database class added

---
+++ KrausxRGA/DBLogics/cmdUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KrausRGA.EntityModel;
+
+namespace KrausRGA.DBLogics
+{
+    /// <summary>
+    /// Avinash : 1Nov2013
+    /// User Get Set functions and other user related functions.
+    /// that is performed on RMASYSTEM database.
+    /// </summary>
+   public class cmdUser
+    {
+
+
+    }
+}
---
+++ KrausxRGA/Models/mUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KrausRGA.Models
+{
+    /// <summary>
+    /// Avinash : 1 Nov 2013.
+    /// contains user information which is logged-in.
+    /// </summary>
+   public class mUser
+    {
+
+       
+
+    }
+}
