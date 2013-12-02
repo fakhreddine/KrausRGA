0dec88b8be77a26e329cb6e529289958fccbad9d
Avinash <apatil@kraususa.com>
2013-12-02 23:05:00 +0530

    Sage entity removed

--- KrausxRGA/Views/RMAInfo.cs
+++ KrausxRGA/Views/RMAInfo.cs
@@ -35,5 +35,70 @@ namespace KrausRGA.Views
         public String State { get; set; }
         public String Country { get; set; }
         public string TCLCOD_0 { get;set; }
+
+        public RMAInfo()
+        {
+
+        }
+
+        public RMAInfo(GetRMAServiceRef.RMAInfoDTO _sage)
+        {
+            if (_sage.RMANumber != null) this.RMANumber = (string)_sage.RMANumber;
+            if (_sage.ShipmentNumber != null) this.ShipmentNumber = (string)_sage.ShipmentNumber;
+            if (_sage.OrderNumber != null) this.OrderNumber = (string)_sage.OrderNumber;
+            if (_sage.PONumber != null) this.PONumber = (string)_sage.PONumber;
+            if (_sage.OrderDate != null) this.OrderDate = (DateTime)_sage.OrderDate;
+            if (_sage.DeliveryDate != null) this.DeliveryDate = (DateTime)_sage.DeliveryDate;
+            if (_sage.ReturnDate != null) this.ReturnDate = (DateTime)_sage.ReturnDate;
+            if (_sage.VendorNumber != null) this.VendorNumber = (string)_sage.VendorNumber;
+            if (_sage.VendorName != null) this.VendorName = (string)_sage.VendorName;
+            if (_sage.SKUNumber != null) this.SKUNumber = (string)_sage.SKUNumber;
+            if (_sage.ProductName != null) this.ProductName = (string)_sage.ProductName;
+            this.DeliveredQty = (int)_sage.DeliveredQty;
+            this.ExpectedQty = (int)_sage.ExpectedQty;
+            this.ReturnedQty = (int)_sage.ReturnedQty;
+            if (_sage.CustomerName1 != null) this.CustomerName1 = (string)_sage.CustomerName1;
+            if (_sage.CustomerName2 != null) this.CustomerName2 = (string)_sage.CustomerName2;
+            if (_sage.Address1 != null) this.Address1 = (string)_sage.Address1;
+            if (_sage.Address2 != null) this.Address2 = (string)_sage.Address2;
+            if (_sage.Address3 != null) this.Address3 = (string)_sage.Address3;
+            if (_sage.ZipCode != null) this.ZipCode = (string)_sage.ZipCode;
+            if (_sage.City != null) this.City = (string)_sage.City;
+            if (_sage.State != null) this.State = (string)_sage.State;
+            if (_sage.Country != null) this.Country = (string)_sage.Country;
+            if (_sage.TCLCOD_0 != null) this.TCLCOD_0 = (string)_sage.TCLCOD_0;
+        }
+
+        public GetRMAServiceRef.RMAInfoDTO CopyToGetDTO(RMAInfo _sage)
+        {
+            GetRMAServiceRef.RMAInfoDTO _return = new GetRMAServiceRef.RMAInfoDTO();
+            if (_sage.RMANumber != null) this.RMANumber = (string)_sage.RMANumber;
+            if (_sage.ShipmentNumber != null) this.ShipmentNumber = (string)_sage.ShipmentNumber;
+            if (_sage.OrderNumber != null) this.OrderNumber = (string)_sage.OrderNumber;
+            if (_sage.PONumber != null) this.PONumber = (string)_sage.PONumber;
+            if (_sage.OrderDate != null) this.OrderDate = (DateTime)_sage.OrderDate;
+            if (_sage.DeliveryDate != null) this.DeliveryDate = (DateTime)_sage.DeliveryDate;
+            if (_sage.ReturnDate != null) this.ReturnDate = (DateTime)_sage.ReturnDate;
+            if (_sage.VendorNumber != null) this.VendorNumber = (string)_sage.VendorNumber;
+            if (_sage.VendorName != null) this.VendorName = (string)_sage.VendorName;
+            if (_sage.SKUNumber != null) this.SKUNumber = (string)_sage.SKUNumber;
+            if (_sage.ProductName != null) this.ProductName = (string)_sage.ProductName;
+            this.DeliveredQty = (int)_sage.DeliveredQty;
+            this.ExpectedQty = (int)_sage.ExpectedQty;
+            this.ReturnedQty = (int)_sage.ReturnedQty;
+            if (_sage.CustomerName1 != null) this.CustomerName1 = (string)_sage.CustomerName1;
+            if (_sage.CustomerName2 != null) this.CustomerName2 = (string)_sage.CustomerName2;
+            if (_sage.Address1 != null) this.Address1 = (string)_sage.Address1;
+            if (_sage.Address2 != null) this.Address2 = (string)_sage.Address2;
+            if (_sage.Address3 != null) this.Address3 = (string)_sage.Address3;
+            if (_sage.ZipCode != null) this.ZipCode = (string)_sage.ZipCode;
+            if (_sage.City != null) this.City = (string)_sage.City;
+            if (_sage.State != null) this.State = (string)_sage.State;
+            if (_sage.Country != null) this.Country = (string)_sage.Country;
+            if (_sage.TCLCOD_0 != null) this.TCLCOD_0 = (string)_sage.TCLCOD_0;
+
+            return _return;
+        }
+
     }
 }
