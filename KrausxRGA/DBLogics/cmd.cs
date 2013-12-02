b8a8412c57e5253c3c972d249ac90120142103b3
Avinash <apatil@kraususa.com>
2013-12-02 18:50:59 +0530

    get and save services added
    save and get services with dynamic end points.

---
+++ KrausxRGA/DBLogics/cmd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KrausRGA.DBLogics
+{
+    /// <summary>
+    /// set service reference endpoints from config file 
+    /// and create new object of each service.
+    /// </summary>
+   public class  cmd
+    {
+       /// <summary>
+       /// Save service object.
+       /// </summary>
+      public static SaveRMAServiceRefer.SaveClient entSave = new SaveRMAServiceRefer.SaveClient(KrausRGA.Properties.Settings.Default.SetServicePath.ToString());
+
+       /// <summary>
+       /// Get Service object.
+       /// </summary>
+      public static GetRMAServiceRef.GetClient entGet = new GetRMAServiceRef.GetClient(KrausRGA.Properties.Settings.Default.GetServicePath.ToString());
+
+      
+       
+    }
+}
